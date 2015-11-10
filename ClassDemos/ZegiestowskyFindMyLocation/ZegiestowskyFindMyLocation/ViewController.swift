8ce24bdece0adbb2da55a77578daf59af3fed62c
ashleyzeg <ashleyzeg@gmail.com>
2015-11-10 11:03:05 -0500

    beginning work on coding with geocode

--- ClassDemos/ZegiestowskyFindMyLocation/ZegiestowskyFindMyLocation/ViewController.swift
+++ ClassDemos/ZegiestowskyFindMyLocation/ZegiestowskyFindMyLocation/ViewController.swift
@@ -7,9 +7,62 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
 
+    @IBAction func displayLocation(sender: AnyObject) {
+        
+        //set the class as delegate for locationManager
+        locationManager.delegate = self
+        
+        //specify location accuracy
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        
+        //display a prompt for authorization
+        locationManager.requestWhenInUseAuthorization()
+        
+        //start receiving location updates from CoreLocation
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //pass location coordinates and check for errors
+        
+        CLGeocoder().reverseGeocodeLocation(manager.location!, completionHandler: { (placemarks, error) -> Void in
+            
+            if (error != nil) {
+                print("Reverse geocoder failed with error: " + error!.localizedDescription)
+                return
+            }
+            
+            //pass the placemark details to displayLocation
+            if placemarks!.count > 0 {
+                let pm = placemarks![0] as CLPlacemark
+                self.displayLocationDetails(pm)
+            } else {
+                print("Problem with the data received from geocoder")
+            }
+        
+        })
+    }
+    
+    func displayLocationDetails(placemark: CLPlacemark?) {
+        if let containsPlacemark = placemark {
+            //stop updating location to save battery life
+            locationManager.startUpdatingLocation()
+            
+            let locality = (containsPlacemark.locality != nil) ? containsPlacemark.locality : ""
+            
+        }
+        
+    }
+    
+    
+    @IBOutlet weak var locationDetails: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
